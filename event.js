2748cfc1df48481842496ca12c7c0e66fae29ae8
cmkwong <cheung.mankwong@yahoo.com.hk>
2020-10-01 16:26:51 +0800

    (35)1. server emit two requests, then observer react these requests

--- event.js
+++ event.js
@@ -1,6 +1,7 @@
 // https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064750?start=19#notes
 
 const EventEmitter = require("events");
+const http = require("http");
 
 // inherited the class EventEmitter
 // https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064750?start=441#notes
@@ -12,7 +13,7 @@ class Sales extends EventEmitter {
 
 const myEmitter = new Sales();
 
-// Observer
+// Observers
 myEmitter.on("newSale", () => {
   console.log("There was a new sale!");
 });
@@ -27,3 +28,30 @@ myEmitter.on("newSale", (stock) => {
 
 // create event (click button)
 myEmitter.emit("newSale", 9);
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const server = http.createServer();
+
+// Obervers
+// meaning the server emit the request twice, and the listener received twice
+// https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064750?start=746#notes
+server.on('request', (req, res) => {
+  console.log("Request received!");
+  // console.log(req.url);
+  res.end("Request received");
+});
+
+server.on('request', (req, res) => {
+  console.log("Another request");
+});
+
+server.on("close", () => {
+  console.log("Server Closed");
+});
+
+// event loop waiting for I/O (Event Emitter)
+// https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064750?start=657#notes
+server.listen(8000, "127.0.0.1", ()=>{
+  console.log("Waiting for requests...");
+});
